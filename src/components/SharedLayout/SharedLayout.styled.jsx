b8742a6adadba9930d66df46a0f6f1600f6deff3
AH400796 <a.hokhman@gmail.com>
2023-07-06 18:45:51 +0300

    Update SharedLayout.styled.jsx

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 import bgMain from '../../images/bg-main.png';
 
-export const AppContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1 0 auto;
-`;
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+`;
+
+export const AppContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
 `;
 
 export const Main = styled.main`
